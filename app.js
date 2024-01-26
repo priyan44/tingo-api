1a3205850c5c294bc0c1bd3392ceab2d1c699a03
Priyan <priyan.arj4@gmail.com>
2024-01-26 23:45:54 +0530

    ip

--- app.js
+++ app.js
@@ -194,8 +194,12 @@ function getDateStr(date) {
 }
 
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
+  });
 
 
